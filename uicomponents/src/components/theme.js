5dfa95db74e0aa07345a267765492f750d0d0ce6
virgile lhomme <virgile.lhomme@gmx.fr>
2020-04-21 10:16:21 +0200

    first commit

---
+++ uicomponents/src/components/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import amber from "@material-ui/core/colors/amber";
+import purple from "@material-ui/core/colors/purple";
+// import lime from "@material-ui/core/colors/lime";
+// import yellow from "@material-ui/core/colors/yellow";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: lime,
+    secondary: yellow,
+  },
+  status: {
+    danger: "red",
+  },
+});
+
+export default theme;
